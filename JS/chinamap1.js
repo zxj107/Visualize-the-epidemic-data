565df619118bc22c1a44312ca88c33886f35068a
zxj107 <67373250+zxj107@users.noreply.github.com>
2020-06-24 20:37:49 +0800

    Add files via upload

---
+++ JS/chinamap1.js
@@ -0,0 +1,75 @@
+var myChart1 = echarts.init(document.querySelector('#chinamap1'))
+      // fetch xmlhttprequest
+      // fetchfetch(请求url)不需要引入任何的文件
+      fetch('data.json')
+        .then(res => res.json()) // 把可读数据流转为json格式
+        .then(res => {
+          console.log(res) // 获取到的疫情数据
+          var getListByCountryTypeService1 = res.getListByCountryTypeService1
+          // 将接口返回的数据进行处理 转为echarts认可的数据
+          var filterData1 = []
+          getListByCountryTypeService1.forEach(item => {
+            filterData1.push({
+              name: item.provinceShortName,
+              value: item.confirmedCount,
+            })          })        
+        myChart1.setOption({
+            title:{
+                text:'累计确诊病例'
+            },
+            tooltip: {
+                    formatter:function(params){
+                        return `地区：${params.name}<br/>累计确诊：${params.value || 0}人`
+                    }//数据格式化
+                },
+            visualMap: [
+              {
+                type: 'piecewise', // continuous连续的 piecewise分段
+                pieces: [
+                  { gte: 10000 }, // (10000, Infinity]
+                  { gte: 1000, lte: 9999 }, // (1000, 9999]
+                  { gte: 100, lte: 999 }, // (100, 999]
+                  { gte: 10, lte: 99 }, // (10, 99]
+                  { gte: 0, lte: 9 }, // (0, 9]
+                  {value:0}
+                ],
+                inRange: {
+                  color: ['#ffffff','#fdebcf', '#f59e83', '#e55a4e', '#cb2a2f', '#811c24']
+                }
+              }
+            ],
+            geo: {
+                map: 'china',
+                roam: false,//不开启缩放和平移
+                zoom:1.23,//视角缩放比例
+                label: {
+                    normal: {
+                        show: true,
+                        fontSize:'10',
+                        color: 'rgba(0,0,0,0.7)'
+                    }
+                },
+                itemStyle: {
+                    normal:{
+                        borderColor: 'rgba(0, 0, 0, 0.2)'
+                    },
+                    emphasis:{
+                        areaColor: '#F3B329',//鼠标选择区域颜色
+                        shadowOffsetX: 0,
+                        shadowOffsetY: 0,
+                        shadowBlur: 20,
+                        borderWidth: 0,
+                        shadowColor: 'rgba(0, 0, 0, 0.5)'
+                    }
+                }
+            },
+            series : [
+                {
+                    name: '确诊人数',
+                    type: 'map',
+                    geoIndex: 0,
+                    data:filterData1
+                }
+            ]
+        })
+    })
---
+++ JS/hubeimap.js
@@ -0,0 +1,79 @@
+var myChart = echarts.init(document.querySelector('#hubeimap'))
+      // fetch xmlhttprequest
+      // fetchfetch(请求url)不需要引入任何的文件
+      fetch('data.json')
+        .then(res => res.json()) // 把可读数据流转为json格式
+        .then(res => {
+          console.log(res) // 获取到的疫情数据
+          var gethubei = res.getAreaStat[0].cities
+          
+          // 将接口返回的数据进行处理 转为echarts认可的数据
+          var filterData = []
+          console.log(filterData)
+          gethubei.forEach(item => {
+            filterData.push({
+              name: item.cityName,
+              value:item.currentConfirmedCount
+            })          })        
+        myChart.setOption({
+            title:{
+                text:'现有确诊病例'
+            },
+            tooltip: {
+                    formatter:function(params){
+                        return `地区：${params.name}<br/>现有确诊：${params.value || 0}人<br/>`
+                    }//数据格式化
+                },
+            visualMap: [
+              {
+                type: 'piecewise', // continuous连续的 piecewise分段
+                pieces: [
+                  { gte: 10000 }, // (10000, Infinity]
+                  { gte: 1000, lte: 9999 }, // (1000, 9999]
+                  { gte: 100, lte: 999 }, // (100, 999]
+                  { gte: 10, lte: 99 }, // (10, 99]
+                  { gte: 0, lte: 9 }, // (0, 9]
+                  {value:0}
+                ],
+                inRange: {
+                   color: ['#ffffff','#fdebcf', '#f59e83', '#e55a4e', '#cb2a2f', '#811c24']
+                }
+              }
+            ],
+            geo: {
+                map: '湖北',
+                  mapType: 'province',
+                roam: false,
+                zoom:0.89,
+                label: {
+                    normal: {
+                        show: true,
+                        fontSize:'10',
+                        color: 'rgba(0,0,0,0.7)'
+                    }
+                },
+                itemStyle: {
+                    normal:{
+                        borderColor: 'rgba(0, 0, 0, 0.2)'
+                    },
+                    emphasis:{
+                        areaColor: '#F3B329',
+                        shadowOffsetX: 0,
+                        shadowOffsetY: 0,
+                        shadowBlur: 20,
+                        borderWidth: 0,
+                        shadowColor: 'rgba(0, 0, 0, 0.5)'
+                    }
+                }
+            },
+            series : [
+                {
+                    name: '确诊人数',
+                    type: 'map',
+                    geoIndex: 0,
+                    data:filterData
+              //      data:filterData
+                }
+            ]
+        })
+      })
